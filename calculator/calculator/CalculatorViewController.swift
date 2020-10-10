cfcd4a5bead7ac943d2e9e6ca58164a2b42e1745
Vitaliy Shapovalov <kupper133@gmail.com>
2020-10-10 21:16:46 +0300

    add: folder with fonts + screenshots + styles all digits + write handle, when user start to write a number

--- calculator/calculator/CalculatorViewController.swift
+++ calculator/calculator/CalculatorViewController.swift
@@ -8,20 +8,39 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+class CalculatorViewController: UIViewController
+{
     @IBOutlet weak var display: UILabel!
+
+    @IBOutlet var digitButtons: [UIButton]!
     
-    @IBOutlet weak var digit: UIButton!
+    var userIsInTheMiddleOfTypingANumber: Bool = false
     
-    @IBAction func appendDigit(_ sender: UIButton) {
+    @IBAction func appendDigit(_ sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber
+        {
+            display.text = display.text! + digit
+        }
+        else
+        {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
     
-    override func viewDidLoad() {
-        digit.layer.shadowColor = UIColor.black.cgColor
-        digit.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
-        digit.layer.shadowOpacity = 0.5
-        digit.layer.masksToBounds = false
-        digit.layer.cornerRadius = digit.frame.width / 2
+    override func viewDidLoad()
+    {
+        for digit in digitButtons
+        {
+            digit.layer.shadowColor = UIColor.black.cgColor
+            digit.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+            digit.layer.shadowOpacity = 0.3
+            digit.layer.masksToBounds = false
+            digit.layer.cornerRadius = digit.frame.width / 2
+        }
     }
 }
 
