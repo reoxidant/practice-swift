99325c1e3c439477132321fe8827e40363df9398
Vitaliy Shapovalov <kupper133@gmail.com>
2020-10-10 19:43:30 +0300

    add: shadow digit button + auto layout label + replace info.plist

--- calculator/calculator/CalculatorViewController.swift
+++ calculator/calculator/CalculatorViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var display: UILabel!
+    
+    @IBOutlet weak var digit: UIButton!
+    
+    @IBAction func appendDigit(_ sender: UIButton) {
     }
-
-
+    
+    override func viewDidLoad() {
+        digit.layer.shadowColor = UIColor.black.cgColor
+        digit.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        digit.layer.shadowOpacity = 0.5
+        digit.layer.masksToBounds = false
+        digit.layer.cornerRadius = digit.frame.width / 2
+    }
 }
 
