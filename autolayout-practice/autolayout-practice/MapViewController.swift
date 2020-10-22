9d0b8b1458981a374d33afc88108f7f3c212ba7b
Vitaliy Shapovalov <kupper133@gmail.com>
2020-10-22 23:30:51 +0300

    add: scroll view in a new view controller + all element on main view + images as a card for scrolling

---
+++ autolayout-practice/autolayout-practice/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  autolayout-practice
+//
+//  Created by Виталий Шаповалов on 22.10.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
