72a104e5310d9f15d5e3e27837696f358b5cecfc
Vitaliy Shapovalov <kupper133@gmail.com>
2020-10-26 12:53:02 +0300

    fix: anchor constraint methods overcoding

--- autolayout-practice/autolayout-practice/WeatherViewController.swift
+++ autolayout-practice/autolayout-practice/WeatherViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class WeatherViewController: UIViewController {
     
+    @IBOutlet private weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,61 +27,26 @@ class WeatherViewController: UIViewController {
             imageView.translatesAutoresizingMaskIntoConstraints = false
             return imageView
         }
-        // [<UILayoutGuide: 0x600002030b60 - "", layoutFrame = {{0, 0}, {0, 0}}, owningView = (null)>, <UILayoutGuide: 0x600002030a80 - "", layoutFrame = {{0, 0}, {0, 0}}, owningView = (null)>]
-        let spacerGuides = (0..<images.count - 1).map
-        {
-            _ in UILayoutGuide()
-        }
-        
-        
-        // <UILayoutGuide: 0x600001e03100 - "", layoutFrame = {{0, 0}, {0, 0}}, owningView = (null)>
-        let containerGuide = UILayoutGuide()
 
-        imageViews.forEach(view.addSubview)
-        spacerGuides.forEach(view.addLayoutGuide)
-        view.addLayoutGuide(containerGuide)
+        imageViews.forEach(stackView.addArrangedSubview)
+        
+        let imageCount = CGFloat(imageViews.count)
+        
+        let constraint = stackView.widthAnchor.constraint(
+            equalTo: imageViews.first!.widthAnchor,
+            multiplier: imageCount,
+            constant: 80 * (imageCount - 1))
+        constraint.priority = UILayoutPriority(749)
+        constraint.isActive = true
         
         
         //container for 3 items of images display on center of screen
         NSLayoutConstraint.activate(
-            [
-                containerGuide.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                containerGuide.leadingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor),
-                containerGuide.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor),
-                {
-                    let constraint = spacerGuides.first!.widthAnchor.constraint(equalToConstant: 80)
-                    constraint.priority = UILayoutPriority(749)
-                    return constraint
-                }(),
-                spacerGuides.last!.widthAnchor.constraint(equalTo: spacerGuides.first!.widthAnchor)
-            ]
             //flatMap return one instance of Array
-            + imageViews.enumerated().flatMap
-            { index, imageView in
-                [
-                    imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                    imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
-                    // | <- [] <- ([]) ?
-                    imageView.leadingAnchor.constraint(
-                        equalTo:
-                        imageView == imageViews.first!
-                        ?
-                        containerGuide.leadingAnchor : spacerGuides[index-1].trailingAnchor
-                    ),
-                     // ([]) ? -> [] -> |
-                    imageView.trailingAnchor.constraint(
-                        equalTo:
-                        imageView == imageViews.last!
-                        ?
-                        containerGuide.trailingAnchor : spacerGuides[index].leadingAnchor)
-                ]
-            }
-            + imageViews.map
-            {
-                $0.widthAnchor.constraint(equalTo: imageViews.first!.widthAnchor)
+            imageViews.map{ imageView in
+                imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
             }
         )
         //dropFirst get sequence container of elements but you can direct first number from last
-        
     }
 }
