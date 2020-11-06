a78dbd3d422a9f6a0316adf093a9ef516194bf27
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-06 16:12:52 +0300

    add: VFL method for align elements but need to do myself

--- autolayout-practice/autolayout-practice/Course Raywenderlich Auto Layout Practice/VFLChallengeViewController.swift
+++ autolayout-practice/autolayout-practice/Course Raywenderlich Auto Layout Practice/VFLChallengeViewController.swift
@@ -13,7 +13,7 @@ class VFLChallengeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let images = [#imageLiteral(resourceName: "sun-with-face"),#imageLiteral(resourceName: "pirate-flag"),#imageLiteral(resourceName: "cloud-with-rain")];
+        let images = [#imageLiteral(resourceName: "sun-with-face"),#imageLiteral(resourceName: "pirate-flag"),#imageLiteral(resourceName: "cloud-with-rain")]
         
         let imageViews:[UIImageView] = images.map{
             image in let imageView = UIImageView.init(image: image)
@@ -21,11 +21,49 @@ class VFLChallengeViewController: UIViewController {
             return imageView
         }
         
-        let spacerGuides = (0..<images.count - 1).map{_ in UILayoutGuide()}
-        let containerGuide = UILayoutGuide()
+        let spaceGuides =  (0..<images.count).map{
+            _ in UILayoutGuide()
+        }
         
         imageViews.forEach(view.addSubview)
-        spacerGuides.forEach(view.addLayoutGuide)
-        view.addLayoutGuide(containerGuide)
+        spaceGuides.forEach(view.addLayoutGuide)
+        
+        let stringVLF = """
+        |[leftGap]\
+        -[monday(fullWidth@849)]-(bigGap)\
+        -[tuesday(smaller@849)]-(gap)\
+        -[wednesday(smaller@849)]-(gap)\
+        -[rightGap(==leftGap)]|
+        """
+        
+        NSLayoutConstraint.activate(
+            NSLayoutConstraint.constraints(
+                withVisualFormat: stringVLF,
+                options: .alignAllBottom,
+                metrics: {
+                    let imageWidth = imageViews[0].image!.size.width
+                    let gap: CGFloat = 10
+                    return [
+                        "fullWidth": imageWidth,
+                        "smaller": imageWidth * 0.7,
+                        "bigGap": gap * 3,
+                        "gap": gap
+                    ]
+                }(),
+                views: Dictionary(uniqueKeysWithValues: [
+                    (["monday", "tuesday", "wednesday"], imageViews),
+                    (["leftGap", "rightGap"], spaceGuides)
+                    ].flatMap({
+                        (arg0) -> Zip2Sequence<[String],[Any]> in
+                        let (keys, values) = arg0 as! ([String], [Any])
+                        return zip(keys,values)
+                    })
+            
+            ))
+            + [imageViews[0].centerYAnchor.constraint(equalTo: view.centerYAnchor)]
+            + imageViews.map({
+                imageView in imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+            })
+        )
     }
 }
