0a06738a779e51541be9d5f216eb02a334f27525
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-06 14:06:19 +0300

    add: stackview autolayout images by center and delete duplicate a code into the file

--- autolayout-practice/autolayout-practice/Course Raywenderlich Auto Layout Practice/AchorStackViewChallengeViewController.swift
+++ autolayout-practice/autolayout-practice/Course Raywenderlich Auto Layout Practice/AchorStackViewChallengeViewController.swift
@@ -11,21 +11,40 @@ import UIKit
 class AchorStackViewChallengeViewController: UIViewController {
     
     @IBOutlet weak var stackView: UIStackView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        let images = [#imageLiteral(resourceName: "cloud-with-rain"),#imageLiteral(resourceName: "sun-with-face"),#imageLiteral(resourceName: "pirate-flag")]
-        
+    
+        let images = [#imageLiteral(resourceName: "cloud-with-rain"),#imageLiteral(resourceName: "pirate-flag"),#imageLiteral(resourceName: "sun-with-face")]
+ 
         let imageViews:[UIImageView] = images.map{
             image in let imageView = UIImageView.init(image: image)
             imageView.translatesAutoresizingMaskIntoConstraints = false
             return imageView
         }
         
-        let countImages = CGFloat(imageViews.count);
+        let countImages = CGFloat(images.count)
+        
+        imageViews.forEach(stackView.addArrangedSubview)
+        
+        let constraintWidth = stackView.widthAnchor.constraint(equalTo: imageViews.first!.widthAnchor, multiplier: countImages, constant: 100 * (countImages - 1))
+        constraintWidth.priority = UILayoutPriority(749)
+        constraintWidth.isActive = true
         
         
+        NSLayoutConstraint.activate(
+            [
+                stackView.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor),
+                stackView.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor),
+            ]
+            + imageViews.flatMap({
+                imageView in
+                [
+                    imageView.widthAnchor.constraint(equalToConstant: 80),
+                    imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+                ]
+            })
+        )
     }
 }
