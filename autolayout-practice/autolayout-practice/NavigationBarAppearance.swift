a07a9de66d866c9634603b02f15f410594cc803a
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-16 11:48:39 +0300

    update #10 issue
    try to resize 3 image, but xcode don't to use my code

---
+++ autolayout-practice/autolayout-practice/NavigationBarAppearance.swift
@@ -0,0 +1,34 @@
+//
+//  NavigationBarApperance.swift
+//  autolayout-practice
+//
+//  Created by Виталий Шаповалов on 15.11.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+enum NavigationBarAppearance {
+    static func setUpTitleText(){
+        struct FontVariation{
+            let horizontalSizeClass: UIUserInterfaceSizeClass
+            let style: String
+            let size: CGFloat
+        }
+        
+        for variation in [
+            FontVariation(horizontalSizeClass: .unspecified, style: "Wide", size:22),
+            FontVariation(horizontalSizeClass: .compact, style: "Thin", size:14)
+            ] {
+                UINavigationBar.appearance(
+                    for: UITraitCollection(horizontalSizeClass: variation.horizontalSizeClass)
+                ).titleTextAttributes = [
+                    NSAttributedString.Key.font: UIFont(
+                        name: "MarkerFelt-\(variation.style)",
+                        size: variation.size
+                        )!
+                ]
+        }
+    }
+}
+
