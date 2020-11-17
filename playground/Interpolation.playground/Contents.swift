ee42bf4343cbad9b0042ae57ed5b3a56ea1483c7
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-17 17:58:34 +0300

    update #17 issue
    Practice with interpolation

---
+++ playground/Interpolation.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var countNumber: Float = 9
+
+var countStringText = "\(countNumber) if delete on 3 will equal to \(countNumber * 3)"
+
+print(countStringText)
+
+var myName = "Vitaliy"
+var myAge = 30
+var city = "Moskow"
+
+print("I am \(myName), \(myAge) years old, i live in \(city)")
