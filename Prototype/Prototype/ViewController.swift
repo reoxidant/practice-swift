c53383c0314a27c74ac4308e893006d391e95a21
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-21 14:33:01 +0300

    close #41 issue
    finish tutorial with basic xcode UI and project files

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Виталий Шаповалов on 21.11.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
