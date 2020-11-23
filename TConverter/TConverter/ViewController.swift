85d4fcc2f5e50dae5064725ba871cd58c2dba8b1
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-23 23:39:35 +0300

    update #159 issue
    Create 1 title +2 label and slider and constrain elements on all screen devices

---
+++ TConverter/TConverter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Виталий Шаповалов on 23.11.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+    }
+}
+
