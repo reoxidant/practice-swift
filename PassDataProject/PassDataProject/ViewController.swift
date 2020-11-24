e87dbf677041ae0c32aed4f517c186a00657f79a
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-24 21:52:40 +0300

    update #161
    create project

---
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Виталий Шаповалов on 24.11.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
