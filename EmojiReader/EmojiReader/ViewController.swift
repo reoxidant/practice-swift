cf0053fa0c5354b9b353bf37d3a59d8df84bc544
Vitaliy Shapovalov <kupper133@gmail.com>
2020-11-26 20:49:29 +0300

    init #42 project
    create project

---
+++ EmojiReader/EmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Виталий Шаповалов on 26.11.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
