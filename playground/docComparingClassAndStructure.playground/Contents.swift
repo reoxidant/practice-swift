d25066a9a3415724bc1193e6cd40a2b4aec08d6c
Vitaliy Shapovalov <kupper133@gmail.com>
2020-12-10 23:23:28 +0300

    Finish #243 issue
    finish my reading and practice on #243 issue, close #243 issue

---
+++ playground/docComparingClassAndStructure.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+//Syntax declaration
+
+class SomeClass{}
+
+struct SomeStructure{}
+
+//example from live experience
+
+struct Profession{
+    var experienceOfYears = 0
+    let name: String?
+    let education: String?
+}
+
+class Human{
+    let age = 0
+    var name: String? = nil
+    let sex: String? = nil
+    var profession = Profession(name: "artist", education: "Master's degree")
+}
+
+//instance Class and Structures
+
+let someClassHuman = Human()
+let someStructureProfession = Profession(name:"Programmer", education: "Student's degree")
+
+//sub topic access to properties
+
+print("The name of profession is \(someClassHuman.profession.name!)")
+
+//in another case
+
+print("The education of human is \(someStructureProfession.education!)")
+
+//change class of properties
+
+someClassHuman.name = "Vitaliy"
+someClassHuman.profession.experienceOfYears = 2
+
+print("The class someClassHuman is changed now here's properties name is \(someClassHuman.name!) and his experience is \(someClassHuman.profession.experienceOfYears) years")
