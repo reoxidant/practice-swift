45a4cf5c6eaf9ce8ae51ebadea84dacf8b104b6d
Vitaliy Shapovalov <kupper133@gmail.com>
2020-12-11 23:58:45 +0300

    finish #249
    finish my practice and learning with Stored Properties. close #249 issue

--- playground/docStoredProperties.playground/Contents.swift
+++ playground/docStoredProperties.playground/Contents.swift
@@ -1,2 +1,28 @@
 import UIKit
 
+//Topic - "Stored Properties"
+
+//describe count students
+
+struct CountStudents{
+    var firstStudent:Int
+    let length:Int
+}
+
+var rangeOfStudents = CountStudents(firstStudent: 0, length: 3)
+
+//now range of students is 0,1,2
+
+rangeOfStudents.firstStudent = 10
+
+//install new range of students 10,11,12
+
+//sub topic - "Stored Properties of Constant Structure Instances"
+
+let secondRangeOfStudent = CountStudents(firstStudent: 5, length: 5)
+
+//now init new range of student that started from 5 to 10
+
+//secondRangeOfStudent.length = 9 //error
+
+//Call error because let is constant type of structure. All strustures depend by type values.
