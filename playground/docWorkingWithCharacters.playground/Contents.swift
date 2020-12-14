0f82b39907b3f4ed724eb97971e0f05a750437d5
Vitaliy Shapovalov <kupper133@gmail.com>
2020-12-14 22:17:37 +0300

    finish #263-issue
    Finish my reading and practice with Characters

---
+++ playground/docWorkingWithCharacters.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//Topic - Working with Characters
+
+//The string constrains from all characters types and we can to through all string using
+//for in cycle
+
+let str = "The test of String"
+
+for ch in str
+{
+    print("\(ch) has a type \(type(of: ch))")
+}
+
+//u can to create character type only use one symbol
+
+let char:Character = "?"
+
+print("Init a new char \(char) has a type \(type(of: char))")
+
+//u can to merge array string items into the one string value
+//for example
+
+let strArray:[Character] = ["I", " ", "a", "m", " ", "i","n", " ", "l", "o", "v", "e" ," ", "M", "a", "r", "y"]
+
+let resToConvertCharToString = String(strArray)
+
+print("I say to you that, \"\(resToConvertCharToString)\"")
