2b2481440f8d4e34b5f5133425a53e7f57edf67c
Vitaliy Shapovalov <kupper133@gmail.com>
2020-12-14 21:56:17 +0300

    Fill and finish #262 issue
    Practice and reading working with Initializing an Empty String

--- playground/docInitializingAnEmptyString.playground/Contents.swift
+++ playground/docInitializingAnEmptyString.playground/Contents.swift
@@ -1,2 +1,15 @@
 import UIKit
 
+//Topic - Initializing an Empty String
+
+var firstEmptyString = "" //empty string literal
+var secondEmptyString = String() //initialization syntax
+
+//U can to check the empty string with lib function isEmpty it's a boolean variable
+
+if firstEmptyString.isEmpty
+{
+    print("It's a empty")
+}
+
+//if firstEmptyString is empty, enter string "It's a empty" in the console
