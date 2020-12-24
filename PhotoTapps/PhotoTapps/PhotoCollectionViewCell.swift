b3405fedacae8a7639b93fb55ddab82dfde3b9e8
Vitaliy Shapovalov <kupper133@gmail.com>
2020-12-24 23:23:57 +0300

    update #43 issue
    Create cell view where descrtibe 15 cells and background them to black color. Create protolol that give me able to change cell calling flowLayout

---
+++ PhotoTapps/PhotoTapps/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoTapps
+//
+//  Created by Виталий Шаповалов on 24.12.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
