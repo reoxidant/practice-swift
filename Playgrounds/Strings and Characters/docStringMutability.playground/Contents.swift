303f2c267e06807db1e098ed5b26478cdaeada8e
Vitaliy Shapovalov <kupper133@gmail.com>
2021-01-06 18:34:09 +0300

    update #349 issue
    Practice and Reading Topic - String Literals, Concatenating Strings, Characters and String Mutability

---
+++ Playgrounds/Strings and Characters/docStringMutability.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Topic - String Mutability\
+
+//in example the variable can be modified in OBJ-C it's have type NSMutableString
+
+let addingText = "Admin"
+var firstString = "Hello "
+firstString += addingText
+
+//in example the constant can't to be modified it's repot a error
+
+let secondString = "Hello "
+//secondString += addingText
