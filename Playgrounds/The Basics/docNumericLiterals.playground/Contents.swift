0e9be228942a3841bc4ed362b1e894132984a6f5
Vitaliy Shapovalov <kupper133@gmail.com>
2021-02-16 23:57:49 +0300

    update practice documentation
    add file reading and practice with topic - Numeric Literals

---
+++ Playgrounds/The Basics/docNumericLiterals.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//Topic - Numeric Literals
+
+//in example write all number literals
+
+let decimalIntegers = 17
+let binaryIntegers = 0b10001
+let octalInteger = 0o21
+let hexadecimalInteger = 0x11
+
+//in decimal value have a double numbers
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+//in example u can to use additional formating use symbols like . or _ for more reading and understanding ables of variables
+
+let paddedDouble = 000123.45
+let million = 1_000_000
+let overThatOneMillion = 1_999_000.000_12_1
+
+print("paddedDouble - \(paddedDouble), million - \(million), overThatOneMillion - \(overThatOneMillion)")
