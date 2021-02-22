7501c83056352c8a1214958951d9af29977f696e
Vitaliy Shapovalov <kupper133@gmail.com>
2021-02-22 22:55:02 +0300

    practice with interface builder
    - use story board to setting many elements autolayout constraints

---
+++ Autolayout/InterfaceBuilder/InterfaceBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterfaceBuilder
+//
+//  Created by Виталий Шаповалов on 22.02.2021.
+//  Copyright © 2021 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
