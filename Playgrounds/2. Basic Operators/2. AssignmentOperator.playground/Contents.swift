850a5a00f0e502e3b3efaf4c3a2dcf8a8cb37d2e
Vitaliy Shapovalov <kupper133@gmail.com>
2021-03-13 23:42:20 +0300

    update the topic Basic Operators
    - rename and fix errors in text

--- Playgrounds/2. Basic Operators/2. AssignmentOperator.playground/Contents.swift
+++ Playgrounds/2. Basic Operators/2. AssignmentOperator.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 //Topic - Assignment Operator
 
-// in example assign a as b now a = b
+//in example assign a as b now a = b
 
 let a = 10
 var b = a
@@ -11,13 +11,12 @@ if a == b{
     print("Success assign")
 }
 
-// example with Tiples
+//example with Tuples
 
 let (x, y) = (5,2)
 
 print("x: \(x), y: \(y)")
 
-
-// how to assign empty value calling errors
+//how to assign empty value calling errors
 
 //let x = y{} //there is nothing, because we assign empty value
