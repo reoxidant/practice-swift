6b21119176ddae3b1ec771f6466af3569258ac0f
Vitaliy Shapovalov <kupper133@gmail.com>
2021-03-13 23:29:49 +0300

    update the basics topic
    - rename and sort the basics topic

--- Playgrounds/The Basics/docSemicolons.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-//Topic - Semicolon
-
-//swift language where don't need to write ; but u can
-
-//; is required if u want to write many expressions on one line
-
-let name:String = "Vitaliy"; let surname:String = "Sokol"; print("My name is \(name) \(surname)");
