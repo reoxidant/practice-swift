281e85050ff0fa509550727fa30773a3a56c765b
Vitaliy Shapovalov <kupper133@gmail.com>
2021-05-21 23:55:42 +0300

    feat(topic): practice and learn the topic - String Interpolation

--- Playgrounds/3. Strings and Characters/6. StringInterpolation.playground/Contents.swift
+++ Playgrounds/3. Strings and Characters/6. StringInterpolation.playground/Contents.swift
@@ -8,16 +8,22 @@ var countStringText = "\(countNumber) if delete on 3 will equal to \(countNumber
 
 print(countStringText)
 
+// 9.0 if delete on 3 will equal to 27.0
+
 var myName = "Vitaliy"
 var myAge = 30
 var city = "Moskow"
 
 print("I am \(myName), \(myAge) years old, i live in \(city)")
 
-//We can entering and counting literals of numbers
+// I am Vitaliy, 30 years old, i live in Moskow
+
+// We can entering and counting literals of numbers
 
 let number = 200
 
 let stringOfCounting = "\(number) / 5 is \(number / 5)"
 
 print(stringOfCounting)
+
+// 200 / 5 is 40
