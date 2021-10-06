672e29eae67b61420428d9fa544ed922e9b0cc03
Vitaliy Shapovalov <kupper133@gmail.com>
2021-10-06 11:49:17 +0300

    Init HelloWorld project

---
+++ Projects/SwiftApps/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Виталий Шаповалов on 06.10.2021.
+//  Copyright © 2021 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
