f8ecc4b3fc6b90d8628c5de411b75d8c9d43a2bf
VitaliiShapovalov <kupper133@gmail.com>
2021-10-13 20:42:33 +0300

    init(AnimalQuiz): initialized the AnimalQuiz project, create main MVC folders

---
+++ Projects/SwiftApps/AnimalQuiz/AnimalQuiz/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AnimalQuiz
+//
+//  Created by hutzo on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
