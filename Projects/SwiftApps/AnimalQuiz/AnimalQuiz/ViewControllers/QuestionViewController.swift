3f7d4387c6e7381c360af399800f172ad1eda25e
Vitaliy Shapovalov <vshapovalov.dev@yandex.ru>
2021-10-19 23:34:07 +0300

    feat(AnimalQuiz): added logic for all question ui elements and show result

--- Projects/SwiftApps/AnimalQuiz/AnimalQuiz/ViewControllers/QuestionViewController.swift
+++ Projects/SwiftApps/AnimalQuiz/AnimalQuiz/ViewControllers/QuestionViewController.swift
@@ -11,7 +11,14 @@ class QuestionViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
-    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet weak var rangedSlider: UISlider! {
+        didSet {
+            let answersCount = Float(currentAnswers.count - 1)
+            let currentValue = answersCount / 2
+            
+            rangedSlider.value = currentValue
+        }
+    }
     
     @IBOutlet weak var singleStackView: UIStackView!
     @IBOutlet weak var multipleStackView: UIStackView!
@@ -19,17 +26,60 @@ class QuestionViewController: UIViewController {
     
     @IBOutlet var singleButtons: [UIButton]!
     @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
     @IBOutlet var rangedLabels: [UILabel]!
     
     // MARK: Properties
     
     private let questions = Question.getQuestion()
     private var questionIndex = 0
+    private var answerChosen = [Answer]()
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateUI()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "showResultSegue" else { return }
+        
+        if let finalVC = segue.destination as? FinalViewController {
+            finalVC.answers = answerChosen
+        }
+    }
+
+    
+    @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
+        guard let currentIndex = singleButtons.firstIndex(of: sender) else { return }
+        
+        let currentAnswer = currentAnswers[currentIndex]
+        answerChosen.append(currentAnswer)
+        
+        nextQuestion()
+    }
+    
+    @IBAction func multipleButtonAnswerPressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answerChosen.append(answer)
+            }
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedButtonAnswerPressed() {
+        
+        let rangedIndex = Int(rangedSlider.value)
+
+        let currentAnswer = currentAnswers[rangedIndex]
+        answerChosen.append(currentAnswer)
+
+        nextQuestion()
     }
 }
 
@@ -56,16 +106,17 @@ extension QuestionViewController {
         title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
         
         // show current StackView
+        showCurrentStackView(for: currentQuestion.type)
     }
     
     private func showCurrentStackView(for type: ResponseType) {
         switch type {
         case .single:
-            break
+            showSingleStackView(with: currentAnswers)
         case .multiple:
-            break
+            showMultipleStackView(with: currentAnswers)
         case .range:
-            break
+            showRangeStackView(with: currentAnswers)
         }
     }
         
@@ -75,5 +126,33 @@ extension QuestionViewController {
         for (button, answer) in zip(singleButtons, answers) {
             button.setTitle(answer.text, for: .normal)
         }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden = false
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.text
+        }
+    }
+    
+    private func showRangeStackView(with answers: [Answer]) {
+        rangeStackView.isHidden = false
+        
+        rangedLabels.first?.text = answers.first?.text
+        rangedLabels.last?.text = answers.last?.text
+    }
+    
+    private func nextQuestion() {
+        let newIndex = questionIndex + 1
+    
+        if newIndex < questions.count {
+            questionIndex = newIndex
+            
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResultSegue", sender: nil)
     }
 }
