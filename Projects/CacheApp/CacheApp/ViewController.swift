a5c08cd9356c47ecf4a473e6633d47b5d770df87
Vitalii Shapovalov <vshapovalov.dev@yandex.ru>
2021-11-23 23:56:51 +0300

    feat(CachedApp): created project

---
+++ Projects/CacheApp/CacheApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CacheApp
+//
+//  Created by Виталий Шаповалов on 22.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
