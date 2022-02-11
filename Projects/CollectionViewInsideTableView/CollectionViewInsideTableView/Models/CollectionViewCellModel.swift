089a93d9abce2dac29d14a40994bc05ce6da4c59
Vitalii Shapovalov <vshapovalov.dev@yandex.ru>
2022-02-11 21:52:05 +0300

    feat(Models): created CollectionViewCellModel

---
+++ Projects/CollectionViewInsideTableView/CollectionViewInsideTableView/Models/CollectionViewCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCellModel.swift
+//  CollectionViewInsideTableView
+//
+//  Created by Виталий Шаповалов on 03.02.2022.
+//
+
+import Foundation
+import UIKit
+
+struct CollectionViewCellModel {
+    var color: UIColor
+    var name: String
+}
