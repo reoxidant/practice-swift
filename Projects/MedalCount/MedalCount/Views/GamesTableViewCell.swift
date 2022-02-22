7c5d880b574e7d364ac2e58e2ebe23fc3351b873
Vitalii Shapovalov <vshapovalov.dev@yandex.ru>
2022-02-22 23:47:40 +0300

    feat(Views): created GamesTableViewCell

---
+++ Projects/MedalCount/MedalCount/Views/GamesTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class GamesTableViewCell: UITableViewCell {
+  // MARK: - IBOutlets
+  @IBOutlet weak var logoImageView: UIImageView!
+  @IBOutlet weak var hostLabel: UILabel!
+  @IBOutlet weak var yearLabel: UILabel!
+}
