f1bc9b99a615d6cd43bbbd92873c35c43494f186
Vitalii Shapovalov <v.shapovalov@napolke.ru>
2022-02-26 19:22:46 +0300

    init(SavannaTours): initialized project

---
+++ Projects/Animations/SavannaTours/SavannaTours/Assets/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SavannaTours
+//
+//  Created by vshapovalov on 26.02.2022.
+//  Copyright © 2022 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+}
