d608f24e2dfec8b23a39372a6806002b291ae5ac
Vitalii Shapovalov <v.shapovalov@napolke.ru>
2022-03-16 19:35:00 +0300

    feat(Spinner): created new spinner animation

--- Projects/Animations/Spinner/Spinner/SpinnerView.swift
+++ Projects/Animations/Spinner/Spinner/SpinnerView.swift
@@ -11,20 +11,28 @@ import SwiftUI
 struct SpinnerView: View {
     
     @State var currentIndex: Int?
+    @State var completed = false
+    @State var isVisible = true
+    @State var currentOffset = CGSize.zero
+    
+    let shootUp = AnyTransition.offset(CGSize(width: 0, height: -1000))
+        .animation(.easeIn(duration: 1))
     
     struct Leaf: View {
         
         let rotation: Angle
-        
         let isCurrent: Bool
+        let isCompleting: Bool
+        
         
         var body: some View {
             Capsule()
                 .stroke(isCurrent ? Color.white: Color.gray, lineWidth: 8)
-                .frame(width: 20, height: 50)
-                .offset(x: 0, y: 70)
-                .rotationEffect(rotation)
-                .animation(.easeInOut(duration: 0.5))
+                .frame(width: 20, height: isCompleting ? 20 : 50)
+                .offset(x: isCurrent ? 10 : 0, y: isCurrent ? 40 : 70)
+                .scaleEffect(isCurrent ? 0.5 : 1)
+                .rotationEffect(isCompleting ? .zero : rotation)
+                .animation(.easeInOut(duration: 1.5))
         }
     }
     
@@ -32,26 +40,67 @@ struct SpinnerView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                ForEach(0 ..< leavesCount) { index in
-                    let degrees = (Double(index) / Double(self.leavesCount)) * 360
-                    let isCurrent = index == self.currentIndex
-                    
-                    Leaf(rotation: Angle(degrees: degrees), isCurrent: isCurrent)
+            if isVisible {
+                ZStack {
+                    ForEach(0 ..< leavesCount) { index in
+                        let degrees = (Double(index) / Double(leavesCount)) * 360
+                        let isCurrent = index == currentIndex
+                        
+                        Leaf(rotation: Angle(degrees: degrees),
+                             isCurrent: isCurrent,
+                             isCompleting: completed
+                        )
+                    }
                 }
+                .onAppear(perform: animate)
+                .transition(shootUp)
+                .gesture(
+                    DragGesture()
+                        .onChanged{ gesture in
+                            currentOffset = gesture.translation
+                        }
+                        .onEnded { gesture in
+                            if currentOffset.height > 150 {
+                                complete()
+                            }
+                            currentOffset = .zero
+                        }
+                )
+                .offset(currentOffset)
+                .blur(radius: currentOffset == .zero ? 0 : 10)
+                .animation(.easeInOut(duration: 1))
             }
-            .onAppear(perform: animate)
         }
     }
     
     func animate() {
+        
+        var iteration = 0
+        
         Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true, block: { timer in
             if let current = currentIndex {
                 self.currentIndex = (current + 1) % self.leavesCount
             } else {
                 self.currentIndex = 0
             }
+            
+            iteration += 1
+            
+            if iteration == 60 {
+                timer.invalidate()
+                self.complete()
+            }
         })
+    }
+    
+    func complete() {
+        guard !completed else { return }
+        
+        completed = true
+        currentIndex = nil
+        delay(seconds: 2) {
+            self.isVisible = false
+        }
     }
 }
 
