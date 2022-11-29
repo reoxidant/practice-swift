5c1ce208e1a043af5477c7b8e714b8e96788fe0e
Vitalii Shapovalov <v.shapovalov@evotor.ru>
2022-11-29 07:16:15 +0300

    refactor(TDD): corrected test case code

--- Playgrounds/TDD/CashRegister.playground/Contents.swift
+++ Playgrounds/TDD/CashRegister.playground/Contents.swift
@@ -1,97 +1,94 @@
 import XCTest
 
 class CashRegister {
+
     var availableFunds: Decimal
     var transactionTotal: Decimal = 0
-    
+
     init(availableFunds: Decimal) {
         self.availableFunds = availableFunds
     }
-    
+
     func addItem(_ cost: Decimal) {
         transactionTotal += cost
     }
-    
-    func acceptCachPayment(_ cash: Decimal) {
+
+    func acceptCashPayment(_ cash: Decimal) {
         transactionTotal -= cash
         availableFunds += cash
     }
 }
 
 class CashRegisterTests: XCTestCase {
-    
-    var sut: CashRegister!
-    
+
     var availableFunds: Decimal!
+    var sut: CashRegister!
     var itemCost: Decimal!
     var payment: Decimal!
-    
-    // 1
+
     override func setUp() {
+
         super.setUp()
-        
         availableFunds = 100
         itemCost = 42
-        payment = 40
+        payment = 50.00
         sut = CashRegister(availableFunds: availableFunds)
     }
-    
-    // 2
+
     override func tearDown() {
+
         availableFunds = nil
-        itemCost = nil
-        payment = nil
         sut = nil
+        itemCost = nil
         super.tearDown()
     }
-    
+
     func testInitAvailableFunds_setsAvailableFunds() {
-        // then
+
         XCTAssertEqual(sut.availableFunds, availableFunds)
     }
-    
+
     func testAddItem_oneItem_addsCostToTransactionTotal() {
-        // when
+
         sut.addItem(itemCost)
-        
-        // then
+
         XCTAssertEqual(sut.transactionTotal, itemCost)
     }
-    
-    func testAddItem_twoItems_addsCostToTransactionTotal() {
-        //given
+
+    func testAddItem_twoItems_addsCostsToTransactionTotal() {
+
         let itemCost2 = Decimal(20)
         let expectedTotal = itemCost + itemCost2
-        
-        // when
+
         sut.addItem(itemCost)
         sut.addItem(itemCost2)
-        
-        // then
+
         XCTAssertEqual(sut.transactionTotal, expectedTotal)
     }
-    
+
     func testAcceptCashPayment_subtractsPaymentFromTransactionTotal() {
-        // given
-        let expected = itemCost - payment
-        
-        // when
-        sut.addItem(itemCost)
-        sut.acceptCachPayment(payment)
-        
-        // then
+
+        doTransaction()
+        let expected = sut.transactionTotal - payment
+
+        sut.acceptCashPayment(payment)
+
         XCTAssertEqual(sut.transactionTotal, expected)
     }
-    
+
     func testAcceptCashPayment_addsPaymentToAvailableFunds() {
-        // given
+
+        doTransaction()
         let expected = sut.availableFunds + payment
+
+        sut.acceptCashPayment(payment)
         
-        // when
-        sut.acceptCachPayment(payment)
-        
-        // then
         XCTAssertEqual(sut.availableFunds, expected)
+    }
+
+    func doTransaction() {
+        sut.addItem(20)
+        sut.addItem(30)
     }
 }
 
